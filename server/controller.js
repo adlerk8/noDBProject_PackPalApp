7739ba303fe264ca901be306fcbe74bd24a7335d
Katie <adlerk8@gmail.com>
2020-10-31 21:34:12 -0700

    server mostly done

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const app = express ();
-const port = 4040;
+const fullPack = require('./pack.json');
+let myPack = [];
 
-app.use(express.json());
-
-
-
-
-
-app.listen(4040, () => console.log(`Server listening on port ${port}`))
+module.exports = {
+    getFullPack: (req, res) => {
+        res.status(200).send(fullPack);
+    },
+    getMyPack: (req, res) => {
+        res.status(200).send(myPack);
+    },
+    editMeds: (req, res) => {
+        const {index} = req.params;
+        const {amMeds, pmMeds} = req.body;
+        myPack[index].amMeds = amMeds;
+        myPack[index].pmMeds = pmMeds;
+        res.status(200).send(myPack);
+    },
+    removeFromPack: (req, res) => {
+        const {index} = req.params;
+        myPack.splice(index, 1);
+        res.status(200).send(myPack);
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express ();
+const port = 4040;
+const ctrl = require('./controller')
+
+
+app.use(express.json());
+
+app.get('/api/FullPackList', ctrl.getFullPack);
+app.get('/api/MyPackList', ctrl.getMyPack);
+app.post('/api/MyPackList/:id', ctrl.editMeds);
+app.delete('/api/MyPackList/:id', ctrl.removeFromPack);
+
+
+
+
+app.listen(4040, () => console.log(`Server listening on port ${port}`))
